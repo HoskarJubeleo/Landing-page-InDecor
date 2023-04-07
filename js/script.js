aa12e474896d39d48964371a6156299fb63f9459
Hoskar Jubeleo de Brito <90986416+HoskarJubeleo@users.noreply.github.com>
2023-04-07 17:58:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+const data = document.querySelector("#data");
+
+const date = new Date();
+const formatter = Intl.DateTimeFormat("pt-BR", {
+    dateStyle: "short",
+});
+
 function href(){
     window.location.href="#subscription-form";
     btnanime.style.animation = 'swing 5s infinite';
 }
+
+data.innerHTML = formatter.format(date);
